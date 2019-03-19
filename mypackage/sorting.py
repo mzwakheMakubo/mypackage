cdf4426b4e6d1e1dc9ab9be8516ec8f51fb1f76e
Mzwakhe Makubo <Mzwakhe@explore-datascience.net>
2019-03-19 12:19:02 +0200

    add sorting

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -13,16 +13,16 @@ def bubble_sort(items):
     '''
 
     exitCondition = 0
-    for i in range(len(myList) - 1):
-        if myList[i] > myList[i + 1]:
-            temp = myList[i]
-            myList[i] = myList[i + 1]
-            myList[i + 1] = temp
+    for i in range(len(items) - 1):
+        if items[i] > items[i + 1]:
+            temp = items[i]
+            items[i] = items[i + 1]
+            items[i + 1] = temp
             exitCondition+=1
     if exitCondition == 0: #return the list if it is already sorted
-        return myList
+        return items
     else:
-        return bubblesort(myList) #calling our function recursively acts as an outer loop with the exitCondition terminating the loop
+        return bubblesort(items) #calling our function recursively acts as an outer loop with the exitCondition terminating the loop
 
 def merge_sort(items):
 
@@ -38,12 +38,12 @@ def merge_sort(items):
         [1,2,3,6,7]
     '''
 
-    arrayLength = len(arr)
+    arrayLength = len(items)
     if arrayLength > 1:
         #we split our array into two seperate arrays
         middle = arrayLength//2
-        leftArray = arr[:middle]
-        rightArray = arr[middle:]
+        leftArray = items[:middle]
+        rightArray = items[middle:]
 
         '''we call our merge_sort function to recursively split our two
         arrays.  '''
@@ -58,10 +58,10 @@ def merge_sort(items):
             #for each iteration, we compare values between two arrays
             #we insert the smallest one in the original array
             if leftArray[leftPos] < rightArray[rightPos]:
-                arr[arrPos] = leftArray[leftPos]
+                items[arrPos] = leftArray[leftPos]
                 leftPos+=1
             else:
-                arr[arrPos] = rightArray[rightPos]
+                items[arrPos] = rightArray[rightPos]
                 rightPos+=1
             arrPos+=1
 
@@ -69,18 +69,18 @@ def merge_sort(items):
         .We start with the leftArray because those values will be less than the values in
         the rightArray'''
         while leftPos < len(leftArray):
-            arr[arrPos] = leftArray[leftPos]
+            items[arrPos] = leftArray[leftPos]
             leftPos+=1
             arrPos+=1
 
         while rightPos < len(rightArray):
-            arr[arrPos] = rightArray[rightPos]
+            items[arrPos] = rightArray[rightPos]
             rightPos+=1
             arrPos+=1
         '''i have not figured out why the remaining leftArray and rightArray values are already sorted.
         I would have loved to give a better explanation but as i continue to understand recursion and the idea of a stack,
         i will update the codebase'''
-    return arr
+    return items
 
 def quick_sort(items):
 
@@ -92,12 +92,12 @@ def quick_sort(items):
             array or list: values are sorted in ascending order.
 
         Examples:
-        >>> bubble_sort([1,7,6,2,3])
+        >>> quick_sort([1,7,6,2,3])
         [1,2,3,6,7]
     '''
 
-    def partitionMyList(items, startingPoint, myPivot):
-        '''partionMyList splits our array/list into two seperate parts divided by myPivot'''
+    def partitionitems(items, startingPoint, myPivot):
+        '''partionitems splits our array/list into two seperate parts divided by myPivot'''
         pivot = items[myPivot] #for this function, are pivot is always the last element in the list (-1)
         i = -1
         for j in range(0, myPivot):
@@ -115,12 +115,12 @@ def quick_sort(items):
 
     def quickSort(items, startingPoint, myPivot):
         if startingPoint < myPivot:
-            piv = partitionMyList(items, startingPoint, myPivot)#our pivot value defined in the function above
+            piv = partitionitems(items, startingPoint, myPivot)#our pivot value defined in the function above
 
             quickSort(items, startingPoint, piv-1)#firts half of list before pivot
             quickSort(items, piv+1, myPivot)#second half of pivot after pivot
         return items
 
-    n = len(myList)
+    n = len(items)
     quickSort(items, 0, n-1)#this forces our function to always assign the last element in the array/list as pivot
     return items
